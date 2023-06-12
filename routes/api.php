8d4a8d1cbc9fc598af7292b5e7b4d79ff95b25f6
EstebanFournier <77848269+EstebanFournier@users.noreply.github.com>
2023-06-12 22:29:19 +0200

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookingSevenAgentController;
 use App\Http\Controllers\VehicleSevenAgentController;
+use App\Http\Controllers\RestituedCarController;
+use App\Http\Controllers\RetiredCarController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,19 +32,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 // Public routes
-
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/booking/{id}', [BookingSevenAgentController::class, 'allById']);
+    Route::get('/users', [AuthController::class, 'index']);
 
-    Route::get('/vehicle/{agency}', [VehicleSevenAgentController::class, 'allById']);
-    Route::put('/vehicle/{agency}', [VehicleSevenAgentController::class, 'update']);
-    Route::post('/vehicle', [VehicleSevenAgentController::class, 'createWithdrawalTicket']);
 
+    //Route::get('/booking/{id}', [BookingSevenAgentController::class, 'allById']);
     Route::get('/booking', [BookingSevenAgentController::class, 'index']);
+    Route::delete('/booking/{id}', [BookingSevenAgentController::class, 'delete']);
+
+    /*Route::get('/vehicle/{agency}', [VehicleSevenAgentController::class, 'allById']);
+    Route::post('/vehicle', [VehicleSevenAgentController::class, 'createWithdrawalTicket']);*/
+    Route::put('/vehicle/{agency}', [VehicleSevenAgentController::class, 'update']);
+
+    Route::get('/retiredCar', [RetiredCarController::class, 'allRetiredCar']);
+    Route::get('/retiredCarForm/{id}', [RetiredCarController::class, 'retiredCarFormById']);
+    Route::put('/retiredCar/update/{id}', [RetiredCarController::class, 'update']);
+    Route::delete('/retiredCar/{id}', [RetiredCarController::class, 'delete']);
+
+    Route::get('/restituedCar', [RestituedCarController::class, 'allRestituedCar']);
+    Route::delete('/restituedCar/{id}', [RestituedCarController::class, 'delete']);
+
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
